f04c87dfad03439fcc6868f8bfbd7e55c836e841
GamendeMier <mauritsmlt@gmail.com>
2019-12-08 20:00:37 +0100

    add basic events

--- games/game.h
+++ games/game.h
@@ -6,6 +6,12 @@ class Game : protected LedControl {
 protected:
     Game(int dataPin, int clkPin, int csPin);
 
+    static float get_axis_x();
+    static float get_axis_y();
+    static bool button_pressed();
+
 public:
+    static void setup();
+
     virtual void update(float delta);
 };
