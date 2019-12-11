e1f1408e25e3ed9ff34fb51db5d3ced4c9fb0af6
GamendeMier <mauritsmlt@gmail.com>
2019-12-11 19:29:42 +0100

    basic snake for testing

--- games/games.ino
+++ games/games.ino
@@ -1,3 +1,4 @@
+/*
 #include "game.h"
 #include "gameselector.h"
 
@@ -10,4 +11,19 @@ void setup() {
 
 void loop() {
     game_selector->update(1.0f);
+}
+*/
+
+#include "game.h"
+#include "snake.h"
+
+Game *game;
+
+void setup() {
+    Game::setup();
+    game = new Snake(13, 11, 10, 1);
+}
+
+void loop() {
+    game->update(0.0000001f);
 }
