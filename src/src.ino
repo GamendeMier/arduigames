cb81c623151a454cce613df24be92cd518f291bd
GamendeMier <mauritsmlt@gmail.com>
2020-01-09 11:52:12 +0100

    add dead

--- src/src.ino
+++ src/src.ino
@@ -12,9 +12,16 @@ void setup() {
 
 int new_time = millis();
 
-void loop() {
-    int old_time = new_time;
-    new_time = millis();
+bool is_dead = false;
 
-    game->update(((float)new_time-(float)old_time)/1000.f);
+void loop() {
+    if (!is_dead) {
+        int old_time = new_time;
+        new_time = millis();
+
+        auto error = game->update(((float)new_time-(float)old_time)/1000.f);
+
+        if (error == GameError::Error)
+            is_dead = true;
+    }
 }
