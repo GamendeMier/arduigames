619d6ed21e2441a0e7b5aacf994c8250d1c40219
GamendeMier <mauritsmlt@gmail.com>
2020-01-09 11:21:05 +0100

    use snake again

--- src/src.ino
+++ src/src.ino
@@ -2,13 +2,13 @@
 #include "gameselector.h"
 #include "snake.h"
 
-GameSelector *game;
+Game *game;
 
 void setup() {
     Game::setup();
     Serial.begin(9600);
 
-    game = new GameSelector();
+    game = new Snake(4);
 }
 
 int new_time = millis();
@@ -17,5 +17,5 @@ void loop() {
     int old_time = new_time;
     new_time = millis();
 
-    game->update(((float)new_time-(float)old_time)/1000);
+    game->update(((float)new_time-(float)old_time)/1000.f);
 }
