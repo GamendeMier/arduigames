94c1eb5f4019943d53929990be498807828c5e59
GamendeMier <mauritsmlt@gmail.com>
2020-01-15 22:15:13 +0100

    bruh moment

--- src/snake.cpp
+++ src/snake.cpp
@@ -92,6 +92,7 @@ GameError Snake::update(float delta) {
         }
 
 		head.x = (float)(int)head.x + 0.5f;
+		head.y = (float)(int)head.y + 0.5f;
 
 		for (int i = 1; i < tail_len; i++) {
 			if (tail[i].x == tail[0].x && tail[i].y == tail[0].y)
